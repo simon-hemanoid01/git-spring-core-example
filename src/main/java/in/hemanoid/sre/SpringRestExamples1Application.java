3481c23c729bd22aa967e5370bb32d86bfd1f7db
Simon Hemanoid <simon.hemanoid@gmail.com>
2022-09-19 20:40:22 +0530

    initial spring setup

---
+++ src/main/java/in/hemanoid/sre/SpringRestExamples1Application.java
@@ -0,0 +1,14 @@
+package in.hemanoid.sre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestExamples1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestExamples1Application.class, args);
+		System.out.println("Hello....");
+	}
+
+}
---
+++ src/test/java/in/hemanoid/sre/SpringRestExamples1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hemanoid.sre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestExamples1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
